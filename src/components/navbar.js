1aa80e519a4d98da4fb3fe8590af3e4b2c286731
Mohit Dhabale <127320469+mohitdhabale@users.noreply.github.com>
2025-05-27 23:46:45 +0530

    Update navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,6 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import food from "../assets/foodsto.png";
-import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 
 function Navget() {
